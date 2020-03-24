165d51b541fa2da78f8f7fcecf754d78aa790eb2
Wilson Neto <wilsonpsn@hotmail.com>
2020-03-24 17:18:41 -0300

    Usa JSON

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 
 const app = express();
-app.get('/', (request, response) => {
-  return response.json({ oi: 'hahhaha' });
+
+app.use(express.json());
+
+/**
+ * Query Param: Filtros, paginação
+ * Routes Params: Parâmetros pra identificar recursos, geralmente usado ara único recurso
+ * Body - Corpo da req, criar ou alterar
+ */
+app.post('/users/', (request, response) => {
+  const params = request.body;
+  return response.json({ oi: params });
 });
 
 app.listen(3333);
