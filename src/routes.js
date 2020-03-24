b0e2f0fb156a499cb46dc5a9b4f57a40d5bdb58a
Wilson Neto <wilsonpsn@hotmail.com>
2020-03-24 19:32:23 -0300

    Get is working

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,12 @@ const routes = express.Router();
  * Body - Corpo da req, criar ou alterar
  */
 
+routes.get('/ongs', async (request, response) => {
+  const ongs = await connection('ongs').select('*');
+
+  return response.json(ongs);
+});
+
 routes.post('/ongs', async (request, response) => {
   const { name, email, whatsapp, city, uf } = request.body;
   const id = crypto.randomBytes(4).toString('HEX');
