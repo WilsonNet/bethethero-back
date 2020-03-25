e1591577bc45c058ba9bf53c5b4c25c07ca0c6c5
Wilson Neto <wilsonpsn@hotmail.com>
2020-03-24 23:46:34 -0300

    Feito delete

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -4,7 +4,7 @@ module.exports = {
   async index(request, response) {
     const incidents = await connection('incidents').select('*');
 
-    return response.json(incidents)
+    return response.json(incidents);
   },
 
   async create(request, response) {
@@ -19,5 +19,25 @@ module.exports = {
     });
 
     return response.json({ id });
+  },
+
+  async delete(request, response) {
+    const { id } = request.params;
+    const ong_id = request.headers.authorization;
+
+    const incident = await connection('incidents')
+      .where('id', id)
+      .select('ong_id').first;
+
+    // Checar se a ONG tem o Incident
+    if (incident.ong_id !== ong_id) {
+      return response.status(401).json({ error: 'Operation not permited.' });
+    }
+
+    await connection('incidents')
+      .where('id', id)
+      .delete();
+
+    return response.status(204).send();
   }
 };
